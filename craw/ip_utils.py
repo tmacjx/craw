73673d48e76d94d326d7cdea70ff9ea4979c2974
tmackan <wangk@bokecc.com>
2020-01-13 16:08:39 +0800

    fffwww

--- craw/ip_utils.py
+++ craw/ip_utils.py
@@ -272,9 +272,10 @@ class IPUtil(object):
 
     def get_random_ip(self):
         # 从数据库中随机获取一个可用的ip
-        before = datetime.datetime.now() - datetime.timedelta(minutes=3)
+        before = datetime.datetime.now() - datetime.timedelta(minutes=5)
         temp_time = datetime.datetime.strftime(before, "%Y-%m-%d %H:%M:%S")
-        random_sql = 'select ip, port from proxy_ip where create_time >= "%s" ORDER BY RAND() LIMIT 1;' % (temp_time, )
+        # random_sql = 'select ip, port from proxy_ip  ORDER BY id desc LIMIT 10;'
+        random_sql = 'select ip, port from proxy_ip where create_time >= "%s" ORDER BY RAND() LIMIT 5;' % (temp_time, )
         # random_sql = 'select ip, port from proxy_ip  ORDER BY RAND() LIMIT 1;'
         # random_sql = 'select ip, port from proxy_ip  ORDER BY create_time desc LIMIT 1;'
 
